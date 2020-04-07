4ff8967470bffd6f8f7dab5fbe4e766907025754
wind1314 <trantrinh0863@gmail.com>
2020-04-07 12:29:30 +0700

    add json properties

--- src/main/java/com/example/demo/dto/BuildingDTO.java
+++ src/main/java/com/example/demo/dto/BuildingDTO.java
@@ -1,5 +1,6 @@
 package com.example.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,21 +14,32 @@ public class BuildingDTO extends AbstractBaseDTO{
 	private String street;
 	private String ward;
 	private String district;
+	@JsonProperty("number_of_basement")
 	private Integer numberOfBasement;
+	@JsonProperty("building_area")
 	private Integer buildingArea;
 	private String structure;
 	private Integer costRent;
+	@JsonProperty("cost_description")
 	private String costDescription;
+	@JsonProperty("service_cost")
 	private String serviceCost;
 	private String carCost;
+	@JsonProperty("motorbike_cost")
 	private String motorbikeCost;
+	@JsonProperty("overtime_cost")
 	private String overtimeCost;
+	@JsonProperty("electricity_cost")
 	private String electricityCost;
 	private String deposit;
 	private String payment;
+	@JsonProperty("time_rent")
 	private String timeRent;
+	@JsonProperty("time_decorator")
 	private String timeDecorator;
+	@JsonProperty("manager_name")
 	private String managerName;
+	@JsonProperty("manager_phone")
 	private String managerPhone;
 	private String type;
 
