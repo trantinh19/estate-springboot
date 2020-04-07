c14201ebd2f892980bc1c3179ac817b29b19de85
wind1314 <trantrinh0863@gmail.com>
2020-04-07 12:31:10 +0700

    add json properties

--- src/main/java/com/example/demo/dto/AbstractBaseDTO.java
+++ src/main/java/com/example/demo/dto/AbstractBaseDTO.java
@@ -1,5 +1,6 @@
 package com.example.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,13 @@ import java.util.Date;
 public class AbstractBaseDTO {
 
     private Long id;
+    @JsonProperty("created_date")
     private Date createdDate;
+    @JsonProperty("modified)_date")
     private Date modifiedDate;
+    @JsonProperty("created_by")
     private String createdBy;
+    @JsonProperty("modified_by")
     private String modifiedBy;
 
 }
